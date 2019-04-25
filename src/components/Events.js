e2712e684ae8a79000839a2691aa90db2b2b1d39
Jorge <julianacero98@gmail.com>
2019-04-25 15:19:46 -0500

    form finished

---
+++ src/components/Events.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Events extends Component {
+  render() {
+    return (
+      <div className="uk-child-width-1-4@m" uk-grid="true">
+        <h1>Events</h1>
+      </div>
+    )
+  }
+}
